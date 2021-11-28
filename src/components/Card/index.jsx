6f07afc3e7d7b86b53e935a007be03cec6c11bf8
unknown <ingeregere@gmail.com>
2021-11-28 11:03:08 +0200

    updated

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,13 +2,14 @@ import { Card, Col, Container, Row,Button } from 'react-bootstrap';
 import moto2 from '../../assets/images/2.jpg'
 import moto4 from '../../assets/images/4.jpg'
 import moto7 from '../../assets/images/7.jpg'
+import './style.css'
 
 function CardProduct() {
   return (
-    <Container fluid className={'mt-5'}>
+    <Container className={'mt-5'}>
     <Row>
       <Col>
-      <Card style={{ width: '18rem' }} className={'mb-3'}>
+      <Card style={{ width: '18rem' }} className={'miseEnpage'}>
   <Card.Img variant="top" src={moto2} />
   <Card.Body>
     <Card.Title  className={'text-primary'}>Business Plan</Card.Title>
@@ -21,7 +22,7 @@ function CardProduct() {
 </Card>
       </Col>
       <Col>
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '18rem' }} className={'miseEnpage'}>
   <Card.Img variant="top" src={moto4} />
   <Card.Body>
     <Card.Title  className={'text-primary'}>Règlement</Card.Title>
@@ -34,7 +35,7 @@ function CardProduct() {
 </Card>
       </Col>
       <Col>
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '18rem' }} className={'miseEnpage'}>
   <Card.Img variant="top" src={moto7} />
   <Card.Body>
     <Card.Title  className={'text-primary'}>Historique</Card.Title>
@@ -47,10 +48,10 @@ function CardProduct() {
 </Card>
       </Col>
       <Col>
-      <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={moto2} />
+      <Card style={{ width: '18rem' }} className={'miseEnpage'}>
+  <Card.Img variant="top" src={moto2} />  
   <Card.Body>
-    <Card.Title className={'text-primary'}>Manuel de procedure</Card.Title>
+    <Card.Title className={'text-primary'}>Manuel de procedure</Card.Title>  
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
